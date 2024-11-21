81b946d78d6a79ade92e0db77f0cc4e3725d23c1
RalfHof <email adresse>
2024-11-21 21:26:11 +0100

    Neu

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-      bucket = "github-actions-tfstate-bucket"
+      bucket = "ralf-state-bucket"
       key = "github-actions.tfstate"
       region = "eu-central-1"
     }
